a69e822e801477958abfff6c15882a74aaa7a70d
Suphavej <fatkimmmm@gmail.com>
2024-12-06 00:44:53 +0700

    finished.2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,14 +1,43 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+string name,movie,day,end;
+int num;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout <<"?????: ";
+    getline(cin,name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";\
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name<<": ";
+    cin >> num;
+
+    cout << "Fahsai: I think you may be GEAR " << (num/10000000)-12 << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cin.ignore();
+    cout << name<<": ";
+    getline(cin,movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name<<": ";
+    getline(cin,day);
+
+    cout <<"Fahsai: "<<day <<"....that is OK!!! I'm looking forward to watching " << movie <<" with you.\n";
+    cout << name<<": ";
+    getline(cin,end);
+
+    cout <<"Fahsai: 555+ see you "<<day<<". Bye Bye \\(^ ^)/";
+    return 0;
+}
+
+
+
 //Dialog of the first example is given below.
 
 /*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
+
 Luke Skywalker: Star Wars VII
 Fahsai: So....which day are you free to go with me?
 Luke Skywalker: Next Monday
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+  // for (size_t i = 1; i < name.length(); i++) {
+   //    if (isspace(name[i]) && isalpha(name[i])) {
+   //       name[i] = toupper(name[i]);
+   //    }
+   // }
